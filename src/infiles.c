e3998d9bc42d8d60aed5b9bea4323e0180f6506f
Jakub <jakub.urbanski115@wp.pl>
2021-03-11 18:53:25 +0100

    poprawa obslugi plikow wejsciowych

--- src/infiles.c
+++ src/infiles.c
@@ -1,33 +1,35 @@
-#include "infliles.h"
+#include "infiles.h"
 
 wrld * make(char* filename){
 	wrld *new_wrld = malloc (sizeof *new_wrld);
 	if(check(filename) == 0){
-		FILE *fin = fopen(filename,"w");
-		fscanf(fin, "%d %d", new_wrld->&r, &new_wrld->&c);
+		FILE *fin = fopen(filename,"r");
+		fscanf(fin, "%d %d", &new_wrld->r, &new_wrld->c);
 		
-		new_wrld->world = malloc(new_wrld->r * sizeof(int));
-
-		for(int i = 0; i < r; i++)
-			new_wrld->world[i] = malloc(new_wrld->c * sizeof(int));
+		new_wrld->world = (int **)malloc(new_wrld->r * sizeof(int*));
 
 		for(int i = 0; i < new_wrld->r; i++)
-			for(int j = 0; j < new_wrld->j; j++)
-				fscanf(fin, "%d", new_wrld->world[i][j]);
+			new_wrld->world[i] = (int *)malloc(new_wrld->c * sizeof(int));
+
+		for(int i = 0; i < new_wrld->r; i++)
+			for(int j = 0; j < new_wrld->c; j++)
+				fscanf(fin, "%d", &new_wrld->world[i][j]);
 
 		return new_wrld;
 	}
 
 	else{
-	       	new_wrld = NULL; // w mainie, jesli new_wrld == NULL to koniec programu :(	
-		return new_wrld;  
+	        // w mainie, jesli new_wrld == NULL to koniec programu :(	
+		return NULL;  
 	}
 }
 
 
 int check(char* filename){
-	int r,c,x;
-	int sum = 0;
+	int r=1;
+	int c=1;
+	int x;
+	int suma = 0;
 
 	FILE * fin = fopen(filename,"r");
 
@@ -57,17 +59,17 @@ int check(char* filename){
 
 
 wrld * copywrld(wrld * w){
-	wrld * new = malloc(sizeof(new));
+	wrld * new = malloc(sizeof *new);
 	new->r = w->r;
 	new->c = w->c;
 
-	new->world = malloc(new->r * sizeof(int));
-
-	for(int i = 0; i < r; i++)
-		new->world[i] = malloc(new->c * sizeof(int));
+	new->world = (int **)malloc(new->r * sizeof(int*));
 
 	for(int i = 0; i < new->r; i++)
-		for(int j = 0; j < new->j; j++)
+		new->world[i] = (int *)malloc(new->c * sizeof(int));
+
+	for(int i = 0; i < new->r; i++)
+		for(int j = 0; j < new->c; j++)
 			new->world[i][j] = w->world[i][j];
 
 	return new;
