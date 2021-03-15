cab0bda5063a85312d93587c883b26d467f20f73
bankowj1 <bania1414@gmail.com>
2021-03-15 18:51:51 +0100

    wyjatki i poprawa działania wyboru zapisu generacji

--- src/main.c
+++ src/main.c
@@ -10,17 +10,17 @@ void help ()
 	char *guide =
 "GUIDE: %s 	-g [liczba generacji]\n"
 "		-s [rodzaj metody sprawdzania sasiedztwa]\n"
-"		-e [nr generacji do zapisania mozna podac max 10 kazdy z prefixem (-e)]\n"
+"		-e [nr generacji do zapisania mozna podac max 10]\n"
 "		-r [rodzaj zasad]\n"	
 "		-b [plik startowy z ...]\n"
 "		-f [folder wyjsciowy]\n"
 "\n";
-	fprintf(stderr,guide);
+	fprintf(stderr,"%s",guide);
 	exit(EXIT_FAILURE);
 }
 
 int compere (const void *a,const void *b)	
-{
+{	
 	return (  *(int*)a - *(int*)b );	
 }
 int main(int argc, char **argv)
@@ -30,10 +30,10 @@ int main(int argc, char **argv)
 	int ruler = 0;
 	int metodaspr = 0;
 	int lend=0;
-	int end[10]={0};
+	int end[11]={0};
 	char *in = NULL;
 	char *out = NULL;
-	
+	int tmp=0;	
 	if(argc<2)
 		help();
 
@@ -48,10 +48,13 @@ int main(int argc, char **argv)
 			metodaspr = atoi(optarg);
 		break;
 		case 'e' ://ktore generacje zapisac
-			end[lend] = atoi(optarg);
-			lend++;
-			//czy dodawac realloc?
-			//kolejne konce musza rownierz zaczynac sie od -e
+			tmp = atoi(optarg);
+			if(tmp>0)
+			{ 
+				end[lend]=tmp;
+				lend++;
+			}else
+			help();
 		break;
 		case 'r' :
 			ruler = atoi(optarg);
@@ -64,22 +67,23 @@ int main(int argc, char **argv)
 		break;
 		}
 	}
-	lend= sizeof(end);
-	end[0]=lg;
-	qsort(end,lend,sizeof(int), compere);//posortowanie wybranych gen do zapisu
+	end[lend]=lg;
+	qsort(end,11,sizeof(int), compere);//posortowanie wybranych gen do zapisu
 	
 	wrld *wld=NULL;
 	wld = make(in);//allocation of the world
 	
 	for(int i=0; i < lg;i++)
 	{
-	int j= 0;
+	int j= 10-lend;
+	
 	spr(metodaspr,ruler,wld);//glowna czesc ktora przetwarza swiat
-		if(i==end[j])
+		if(i==(end[j]-1))
 		{
 			printIt(wld,out,i);//savestan
 			j++;
 		}
+		
 		//grafika
 	}
 	freewrld(wld);
--- src/spr.h
+++ src/spr.h
@@ -9,7 +9,7 @@ int moon (int ruler, int i, int j, wrld * wld);
 int vonn (int ruler, int i, int j, wrld * wld);
 int (*sprfunk[2])(int , int ,int , wrld *);
 void spr (int metodaspr,int rules, wrld * wld);
-int add (int i, int j, int k, int l, wrld * wld);
+int add (int i, int j, int l, int k, wrld * wld);
 int (*rules[1])(int , int );
 
 #endif
